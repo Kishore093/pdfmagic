13f4451014e7b09c44ced7a1105b433db1730e68
Kishore093 <Kishorepatidar092@gmail.com>
2024-07-06 18:59:32 +0530

    update data

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "20bb4836e955106b0019d406dac7d032
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "4888c4f2137b438e4e52e333e3a232be",
+"flutter_bootstrap.js": "96b86cd8ff9d1b426e89666560b2d79c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
