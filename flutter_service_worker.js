84396e36a90c91f8dd4f12a1dbc55c9d3d147078
Kishore093 <Kishorepatidar092@gmail.com>
2024-07-12 19:07:09 +0530

    Add pdf Extension

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,14 +31,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a97699fab9b9a388ea8047bff3086681
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "6d223154c1519e8a788193fb675930df",
+"flutter_bootstrap.js": "0fe75b4d279d671ac431b88e24dc69f9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "8581b0f74c94e30d02cc8f4da4f12223",
 "/": "8581b0f74c94e30d02cc8f4da4f12223",
-"main.dart.js": "e6f39e41a7fbf15e107f00169e551514",
+"main.dart.js": "0fea0f1954a606efdd02989d2e54400f",
 "manifest.json": "1a77ba1d121839c7cd04ba6524479958",
 "version.json": "1ceca087fadc22e27e95e2388fe65bbe"};
 // The application shell files that are downloaded before a service worker can
